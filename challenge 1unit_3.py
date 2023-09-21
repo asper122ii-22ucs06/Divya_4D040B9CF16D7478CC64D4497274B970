ea594b54c00576e855b24410e0585ef80fc5aefa
asper122ii-22ucs06 <144366827+asper122ii-22ucs06@users.noreply.github.com>
2023-09-21 20:14:49 +0530

    Add files via upload

---
+++ challenge 1unit_3.py
@@ -0,0 +1,12 @@
+def linearSearchProduct(productlist, targetproduct):
+  indices = []  
+  for index, product in enumerate(productlist):
+     if product==targetproduct:
+      indices.append(index)
+  return indices
+#example usage:
+  (variable) : list[str]
+product=["shoes", "boot", "loafer", "shoes", "sandal", "shoes"]
+target = "shoes"
+result = linearSearchProduct(product,target)
+print (result)
